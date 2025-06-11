224edbf95521c830f6f9e9ecf47e8d5a43fc39f4
ParsaProg <parsashaabani3@gmail.com>
2025-06-12 01:44:33 +0330

    add env file for project

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -4,8 +4,8 @@ import GitHubProvider from "next-auth/providers/github";
 const handler = NextAuth({
   providers: [
     GitHubProvider({
-      clientId: "Ov23liT9doipw77pcH46",
-      clientSecret: "47d0c432d7c5c15b63965a1a788aec4c9fea2010",
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
     }),
   ],
 });
