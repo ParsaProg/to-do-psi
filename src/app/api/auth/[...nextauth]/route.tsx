2f5d47a8a2975e26120ac6738d0185411041a228
ParsaProg <parsashaabani3@gmail.com>
2025-06-12 21:44:33 +0330

    update3 gitingore file to dont push .env.local file

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -2,6 +2,16 @@ import NextAuth from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        httpOnly: true,
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      },
+    },
+  },
   providers: [
     GitHubProvider({
       clientId: process.env.GITHUB_ID!,
